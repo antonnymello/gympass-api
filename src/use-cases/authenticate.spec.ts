65bbce9e76ff79b1d4c2c7e51faae00012224ecc
antonny melo <antonny.mello@gmail.com>
2023-12-12 18:06:39 -0300

    refactor: improve Authenticate test quality

--- src/use-cases/authenticate.spec.ts
+++ src/use-cases/authenticate.spec.ts
@@ -1,14 +1,19 @@
-import { describe, it, expect } from 'vitest';
+import { describe, it, expect, beforeEach } from 'vitest';
 import { InMemoryUsersRepository } from '@/repositories/in-memory/in-memory-users-repository';
 import { AuthenticateUseCase } from '@/use-cases/authenticate';
 import { hash } from 'bcryptjs';
 import { InvalidCredentialsError } from '@/use-cases/errors/invalid-credentials-error';
 
 describe('Authenticate Use Case', () => {
-  it('should be able to authenticate', async () => {
-    const userRepository = new InMemoryUsersRepository();
-    const sut = new AuthenticateUseCase(userRepository);
+  let userRepository: InMemoryUsersRepository;
+  let sut: AuthenticateUseCase;
 
+  beforeEach(() => {
+    userRepository = new InMemoryUsersRepository();
+    sut = new AuthenticateUseCase(userRepository);
+  });
+
+  it('should be able to authenticate', async () => {
     await userRepository.create({
       name: 'any_name',
       email: 'any_email@mail.com',
@@ -24,9 +29,6 @@ describe('Authenticate Use Case', () => {
   });
 
   it('should not be able to authenticate with wrong email', async () => {
-    const userRepository = new InMemoryUsersRepository();
-    const sut = new AuthenticateUseCase(userRepository);
-
     const promise = sut.execute({
       email: 'invalid_email@mail.com',
       password: 'any_password',
@@ -36,14 +38,11 @@ describe('Authenticate Use Case', () => {
   });
 
   it('should not be able to authenticate with wrong password', async () => {
-    const userRepository = new InMemoryUsersRepository();
-    const sut = new AuthenticateUseCase(userRepository);
-
     await userRepository.create({
-        name: 'any_name',
-        email: 'any_email@mail.com',
-        password_hash: await hash('any_password', 6),
-      });
+      name: 'any_name',
+      email: 'any_email@mail.com',
+      password_hash: await hash('any_password', 6),
+    });
 
     const promise = sut.execute({
       email: 'any_email@mail.com',
