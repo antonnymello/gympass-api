9bb77f86c7d48837468b3190fa2b15600e99db13
antonny melo <antonny.mello@gmail.com>
2024-02-16 18:44:12 -0300

    feat: add auth middleware on route

--- src/http/routes.ts
+++ src/http/routes.ts
@@ -2,10 +2,12 @@ import { type FastifyInstance } from 'fastify';
 import { register } from '@/http/controllers/register.controller';
 import { authenticate } from '@/http/controllers/authenticate';
 import { profile } from '@/http/controllers/profile';
+import { verifyJWT } from '@/http/middlewares/verifiy-jwt';
 
 export const appRoutes = async (app: FastifyInstance): Promise<void> => {
   app.post('/users', register);
   app.post('/sessions', authenticate);
 
-  app.get('/me', profile);
+  // Authenticated routes
+  app.get('/me', { onRequest: [verifyJWT] }, profile);
 };
