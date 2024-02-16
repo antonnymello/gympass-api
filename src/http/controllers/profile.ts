ea26e599ace9384e6be6de015b135cf8c0e7301d
antonny melo <antonny.mello@gmail.com>
2024-02-16 18:17:54 -0300

    feat: add fastify jwt

---
+++ src/http/controllers/profile.ts
@@ -0,0 +1,6 @@
+import { type FastifyRequest, type FastifyReply } from 'fastify';
+
+export const profile = async (
+  request: FastifyRequest,
+  reply: FastifyReply
+): Promise<void> => {};
--- src/http/routes.ts
+++ src/http/routes.ts
@@ -1,8 +1,11 @@
 import { type FastifyInstance } from 'fastify';
 import { register } from '@/http/controllers/register.controller';
 import { authenticate } from '@/http/controllers/authenticate';
+import { profile } from '@/http/controllers/profile';
 
 export const appRoutes = async (app: FastifyInstance): Promise<void> => {
   app.post('/users', register);
   app.post('/sessions', authenticate);
+
+  app.get('/me', profile);
 };
