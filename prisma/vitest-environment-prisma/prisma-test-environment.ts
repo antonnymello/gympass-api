07ade59a4436028679cb28969cd8426dc959b905
antonny melo <antonny.mello@gmail.com>
2024-06-13 09:25:08 -0300

    feat: Update prisma-test-environment.ts to generate a unique database URL

--- prisma/vitest-environment-prisma/prisma-test-environment.ts
+++ prisma/vitest-environment-prisma/prisma-test-environment.ts
@@ -1,9 +1,30 @@
+import { execSync } from 'node:child_process';
+import { randomUUID } from 'node:crypto';
 import { type Environment } from 'vitest';
+
+const generateDatabaseURL = (schema: string): string => {
+  const databaseURL = process.env.DATABASE_URL;
+
+  if (!databaseURL) {
+    throw new Error('DATABASE_URL is not set');
+  }
+
+  const url = new URL(databaseURL);
+
+  url.searchParams.set('schema', schema);
+
+  return url.toString();
+};
 
 const prismaEnvironment: Environment = {
   name: 'prisma',
   async setup() {
-    console.log('Running...');
+    const schema = randomUUID();
+    const databaseURL = generateDatabaseURL(schema);
+
+    process.env.DATABASE_URL = databaseURL;
+
+    execSync('npx prisma migrate deploy');
 
     return {
       async teardown() {
